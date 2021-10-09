71975dc3fb7c568183babcfbf4d0380a949cda48
githublaure <laureagrech@gmail.com>
2021-10-10 00:20:02 +0200

    bootstrap css

--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,21 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="Weather">
-      <H1>Paris</H1>
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="name your city"
+              className="form-control"
+            />
+          </div>
+          <div className="col-9">
+            <input type="submit" value="Search" className="btn btn-primary" />
+          </div>
+        </div>
+      </form>
+      <h1>Paris</h1>
       <ul>
         <li>Wednesday 07:00</li>
         <li>Mostly Cloudy</li>
