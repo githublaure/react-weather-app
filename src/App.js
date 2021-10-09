56e4e202c4c303e006e92e5bbf5b3c18dd6b0e1c
githublaure <laureagrech@gmail.com>
2021-10-09 21:33:39 +0200

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,19 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github/githublaure/" target="_blank">
+          Laure Agrech
+        </a>{" "}
+        and is{" "}
+        <a href="https://github/githublaure/react-weather-app" target="_blank">
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
