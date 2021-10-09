679815ef28f124838c99490de80c097790ec01a3
githublaure <laureagrech@gmail.com>
2021-10-09 21:38:51 +0200

    links edit

--- src/App.js
+++ src/App.js
@@ -7,11 +7,14 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project was coded by{" "}
-        <a href="https://github/githublaure/" target="_blank">
+        <a href="https://github.com/githublaure/" target="_blank">
           Laure Agrech
         </a>{" "}
         and is{" "}
-        <a href="https://github/githublaure/react-weather-app" target="_blank">
+        <a
+          href="https://github.com/githublaure/react-weather-app-laure"
+          target="_blank"
+        >
           open-sourced on GitHub
         </a>
       </footer>
