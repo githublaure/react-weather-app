6f773bc95ea155c16ad22a5104ed8aa0a30415fe
githublaure <laureagrech@gmail.com>
2021-10-13 12:06:07 +0200

    minor fixes

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -22,10 +22,11 @@ export default function WeatherIcon(props) {
     "50d": "FOG",
     "50n": "FOG",
   };
+
   return (
     <ReactAnimatedWeather
       icon={codeMapping[props.code]}
-      color="black"
+      color="#1e1e1e"
       size={props.size}
       animate={true}
     />
